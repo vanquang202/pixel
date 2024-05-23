a437524e1fb3d712d7512b3271101c9261bfa0bb
JanJoang <83849198+vanquang202@users.noreply.github.com>
2024-05-23 21:17:26 +0700

    first commit

---
+++ app/Http/Middleware/AuthFake.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthFake
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = new User();
+        $user->id = rand();
+        $user->name = "Member";
+        $request->merge(['user' => $user]);
+        $request->setUserResolver(function () use ($user) {
+            return $user;
+        });
+
+        return $next($request);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('get-pixel', function () {
+    $gridSize = 5;
+    $canvasHeight = 3000;
+    $canvasWidth = 8000;
+    $rows = $canvasHeight / $gridSize;
+    $cols = $canvasWidth / $gridSize;
+
+    if (Cache::getRedis()->exists("pixel")) {
+        $data = json_decode(Cache::getRedis()->get("pixel"));
+        return response()->json([
+            "data" => $data,
+            "gridSize" =>  $gridSize,
+            "canvasHeight" =>  $canvasHeight,
+            "canvasWidth" =>  $canvasWidth,
+            "rows" =>  $rows,
+            "cols" =>  $cols,
+        ]);
+    }
+    $data =  [];
+    for ($row = 0; $row < $rows; $row++) {
+        $data[$row] = [];
+        for ($col = 0; $col < $cols; $col++) {
+            $data[$row][$col] = false;
+        }
+    }
+    Cache::getRedis()->set("pixel", json_encode($data), "ex", 6 * 30 * 24 * 60 * 60);
+    return response()->json([
+        "data" => $data,
+        "gridSize" =>  $gridSize,
+        "canvasHeight" =>  $canvasHeight,
+        "canvasWidth" =>  $canvasWidth,
+        "rows" =>  $rows,
+        "cols" =>  $cols,
+    ]);
+});
+Route::get('send-pixel', function () {
+    $data = json_decode(Cache::getRedis()->get("pixel"));
+    $data[request()->clickedRow][request()->clickedCol] = "#" . request()->selectedColor;
+    Cache::getRedis()->set("pixel", json_encode($data), "ex", 6 * 30 * 24 * 60 * 60);
+});
