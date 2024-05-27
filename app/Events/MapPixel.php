202d8c5c5e56ef595d9d05a59ea057936e71fe6f
JanJoang <83849198+vanquang202@users.noreply.github.com>
2024-05-27 19:18:47 +0700

    a

--- app/Events/MapPixel.php
+++ app/Events/MapPixel.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MapPixel
+class MapPixel implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
