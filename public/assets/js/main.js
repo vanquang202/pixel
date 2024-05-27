8abb9691da92dc1c1544cc8391750128025d59e3
JanJoang <83849198+vanquang202@users.noreply.github.com>
2024-05-27 16:05:06 +0700

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -70,41 +70,45 @@ function start() {
             .catch((err) => {});
 
         function run() {
-            echo.listenForWhisper("call", async (event) => {
-                const chunkSize = 50;
-                for (let i = 0; i < grid.length; i += chunkSize) {
-                    const chunk = grid.slice(i, i + chunkSize);
+            echo.listenForWhisper("call", (event) => {
+                async function sendData() {
+                    const chunkSize = 50;
+                    for (let i = 0; i < grid.length; i += chunkSize) {
+                        const chunk = grid.slice(i, i + chunkSize);
+                        await echo.whisper("client-" + event.idU, {
+                            grid: JSON.stringify(chunk),
+                        });
+                    }
                     await echo.whisper("client-" + event.idU, {
-                        grid: JSON.stringify(chunk),
+                        isDone: true,
+                        canvasHeight: canvasHeight,
+                        canvasWidth: canvasWidth,
+                        gridSize: gridSize,
+                        rows: rows,
+                        cols: cols,
                     });
                 }
-                await echo.whisper("client-" + event.idU, {
-                    isDone: true,
-                    canvasHeight: canvasHeight,
-                    canvasWidth: canvasWidth,
-                    gridSize: gridSize,
-                    rows: rows,
-                    cols: cols,
-                });
-            }).listenForWhisper("send-client", (event) => {
-                grid[event.clickedRow][event.clickedCol] = event.selectedColor;
-                context.fillStyle = event.selectedColor;
-                context.fillRect(
-                    event.col,
-                    event.row,
-                    event.gridSize,
-                    event.gridSize
-                );
-                // mouse
-                if (!document.getElementById(event.idU))
-                    createCursor(event.idU);
-                const cursor = document.getElementById(event.idU);
-                cursor.style.setProperty("--mouseX", event.mouseX + "px");
-                cursor.style.setProperty("--mouseY", event.mouseY + "px");
-                setTimeout(() => {
-                    cursor.remove();
-                }, 1000);
+                sendData();
             });
+            // .listenForWhisper("send-client", (event) => {
+            //     grid[event.clickedRow][event.clickedCol] = event.selectedColor;
+            //     context.fillStyle = event.selectedColor;
+            //     context.fillRect(
+            //         event.col,
+            //         event.row,
+            //         event.gridSize,
+            //         event.gridSize
+            //     );
+            //     // mouse
+            //     if (!document.getElementById(event.idU))
+            //         createCursor(event.idU);
+            //     const cursor = document.getElementById(event.idU);
+            //     cursor.style.setProperty("--mouseX", event.mouseX + "px");
+            //     cursor.style.setProperty("--mouseY", event.mouseY + "px");
+            //     setTimeout(() => {
+            //         cursor.remove();
+            //     }, 1000);
+            // });
 
             let colorPicker = document.getElementById("colorPicker");
             let selectedColor = colorPicker.value;
@@ -112,44 +116,36 @@ function start() {
                 selectedColor = event.target.value;
             });
 
-            canvas.addEventListener("mousedown", async function (event) {
-                const audio = document.createElement("video");
-                audio.src = "/assets/mp3/click.mp3";
-                audio.play();
-                let rect = canvas.getBoundingClientRect();
-                let mouseX = event.clientX - rect.left;
-                let mouseY = event.clientY - rect.top;
+            // canvas.addEventListener("mousedown", async function (event) {
+            //     const audio = document.createElement("video");
+            //     audio.src = "/assets/mp3/click.mp3";
+            //     audio.play();
+            //     let rect = canvas.getBoundingClientRect();
+            //     let mouseX = event.clientX - rect.left;
+            //     let mouseY = event.clientY - rect.top;
 
-                let clickedRow = Math.floor(mouseY / gridSize);
-                let clickedCol = Math.floor(mouseX / gridSize);
-                grid[clickedRow][clickedCol] = selectedColor;
-                context.fillStyle = selectedColor;
-                context.fillRect(
-                    clickedCol * gridSize,
-                    clickedRow * gridSize,
-                    gridSize,
-                    gridSize
-                );
-                // await fetch(
-                //     `${url}/api/send-pixel?clickedRow=${clickedRow}&clickedCol=${clickedCol}&selectedColor=${selectedColor.replace(
-                //         "#",
-                //         ""
-                //     )}`
-                // )
-                //     .then((res) => {})
-                //     .catch((err) => location.reload());
-                await echo.whisper("send-client", {
-                    col: clickedCol * gridSize,
-                    row: clickedRow * gridSize,
-                    gridSize: gridSize,
-                    selectedColor: selectedColor,
-                    idU: idU,
-                    mouseX: mouseX,
-                    mouseY: mouseY,
-                    clickedRow: clickedRow,
-                    clickedCol: clickedCol,
-                });
-            });
+            //     let clickedRow = Math.floor(mouseY / gridSize);
+            //     let clickedCol = Math.floor(mouseX / gridSize);
+            //     grid[clickedRow][clickedCol] = selectedColor;
+            //     context.fillStyle = selectedColor;
+            //     context.fillRect(
+            //         clickedCol * gridSize,
+            //         clickedRow * gridSize,
+            //         gridSize,
+            //         gridSize
+            //     );
+            //     await echo.whisper("send-client", {
+            //         col: clickedCol * gridSize,
+            //         row: clickedRow * gridSize,
+            //         gridSize: gridSize,
+            //         selectedColor: selectedColor,
+            //         idU: idU,
+            //         mouseX: mouseX,
+            //         mouseY: mouseY,
+            //         clickedRow: clickedRow,
+            //         clickedCol: clickedCol,
+            //     });
+            // });
 
             function drawGrid() {
                 context.clearRect(0, 0, canvasWidth, canvasHeight);
