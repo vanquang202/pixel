b71a5fd5f374b5a1b62498b026a82f246fff28c3
JanJoang <83849198+vanquang202@users.noreply.github.com>
2024-05-27 23:26:27 +0700

    aaa

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\AuthFake;
+use App\Http\Middleware\CheckAuth;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -64,6 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        "user.fake" => AuthFake::class
+        "user.fake" => AuthFake::class,
+        "check.auth" => CheckAuth::class
     ];
 }
